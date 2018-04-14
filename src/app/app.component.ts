0eaf05176aa4ad7e8a0739e69127a4cabdf32148
zainhatim <zain.hatim@qnexis.com>
2018-04-14 18:16:32 -0400

    begins here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import {DrawableDirective} from './drawable.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  @ViewChild(DrawableDirective) canvas;
+  model: tf.Model;
+  predictions: any;
+
+  ngOnInit(): void {
+    this.loadModel();
+  }
+
+  async loadModel() {
+    this.model = await tf.loadModel('/assets/mnist_model_js/model.json');
+  }
+
+  async predict(imageData: ImageData) {
+    const prediction = await tf.tidy(() => {
+      let img = tf.fromPixels(imageData, 1);
+      img = img.reshape([1, 28, 28, 1]);
+      img = tf.cast(img, 'float32');
+      const output = this.model.predict(img) as any;
+
+      this.predictions = Array.from(output.dataSync());
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChartComponent} from './chart/chart.component';
+import { DrawableDirective } from './drawable.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponent,
+    DrawableDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
